4eb5debfeaee63f18f93b9655528bb0bceb94def
Supriya-Sirigineedi <138785192+Supriya-Sirigineedi@users.noreply.github.com>
2023-08-11 19:15:03 +0530

    Add files via upload

---
+++ Selenium/Project/Activity2.py
@@ -0,0 +1,14 @@
+# Import webdriver from selenium
+from selenium import webdriver
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
+
+# Set up the Firefox Driver with WebDriverManger
+service = FirefoxService(GeckoDriverManager().install())
+
+# Start the Driver
+with webdriver.Firefox(service=service) as driver:
+    # Navigate to the URL
+    driver.get("http://alchemy.hguy.co/orangehrm")
+    # Print the title of the page
+    print("URL of the header image ", driver.current_url)
